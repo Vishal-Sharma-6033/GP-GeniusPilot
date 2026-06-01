b0378a7b05fd755566bd38413c947a0edaa00c5c
Vishal Sharma <vs2520329@gmail.com>
2026-06-01 11:55:58 +0530

    feat: add auth middleware

---
+++ Backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,49 @@
+const jwt = require("jsonwebtoken")
+const tokenBlacklistModel = require("../models/blacklist.model")
+
+
+
+async function authUser(req, res, next) {
+
+    const token = req.cookies.token
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Token not provided."
+        })
+    }
+
+    const isTokenBlacklisted = await tokenBlacklistModel.findOne({
+        token
+    })
+
+    if (isTokenBlacklisted) {
+        return res.status(401).json({
+            message: "token is invalid"
+        })
+    }
+
+    try {
+        if (!process.env.JWT_SECRET) {
+            return res.status(500).json({
+                message: "Auth configuration is missing"
+            })
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        req.user = decoded
+
+        next()
+
+    } catch (err) {
+
+        return res.status(401).json({
+            message: "Invalid token."
+        })
+    }
+
+}
+
+
+module.exports = { authUser }
